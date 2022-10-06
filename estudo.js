2a3f9e948ffb7cbeb980f738b13351fb7cadecd1
caio46644 <108937026+caio46644@users.noreply.github.com>
2022-10-06 11:13:52 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -12,3 +12,19 @@ function escreva(){
     document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
+
+
+let lista = ["jesus","spessato","jorge","duarte","waldete","raquel"]
+
+function mostra(){
+    document.write(lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write(lista[0] + "<br>")
+    }
+}
+
+function tabuada(){
+    for(let i = 1; i <= 10; i++){
+        document.write("o valor do i " + i + "<br>")
+    }
+}
